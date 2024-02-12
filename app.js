99086ffcd737d0c28df63be651db7f7a43725a16
Jeff LeBoeuf <jeff@jeff.how>
2024-02-12 09:02:29 -0500

    more

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const secret = "aback";
+const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+
+const alpha = new Set(letters.split(''));
+
+
+window.addEventListener('keyup', logKey);
+
+
+function logKey(evt) {
+    if (alpha.has( evt.key.toUpperCase() )) {
+        console.log(evt.key.toUpperCase());
+    } else {
+        console.log('not a letter');
+    }
+}
+
