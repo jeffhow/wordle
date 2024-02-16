3f728342914800cccee96016991516f803546f36
Jeff LeBoeuf <jeff@jeff.how>
2024-02-16 12:26:53 -0500

    board

--- app.js
+++ app.js
@@ -1,78 +1,80 @@
-// Create a variable to hold the secret.
-// Const can't be changed later
 const secret = "aback";
 
-// create all the letters in uppercase
 const letters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
-
-// letters.split('') => This splits the letters into an Array
-// a new Set is like an array, but only contains unique values
 const alpha = new Set(letters.split(''));
 
-// attach a function to the browser window.
-// when a key is released (keyup), the logkey() 
-// function will be called
-// the evt is "Keyup" and the callback function is "LogKey"
+let tries = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    [], // 6 tries
+];
+
+let attempt = 0;
+
 window.addEventListener('keyup', logKey);
 
-// This is the code for the logKey function
-// Whenever an event fires, and a callback is triggered
-// that event is actually sent to the callback as a JS object.
-function logKey(evt) { // evt is the event. 
-    // all this stuff is the saved program
-    // this runs every time logKey is called()
+function logKey(evt) { 
     console.log(evt);
 
-    // check to see if the enter key was typed
     if (evt.key.toUpperCase() == "ENTER") {
         console.log('submit');
+        if (attempt < 6 && tries[attempt].length == 5) {
+            attempt++;
+        }
     }
 
-    // check to see if the backspace was types
     if (evt.key.toUpperCase() == "BACKSPACE") {
         console.log('deleting');
+        tries[attempt].pop();
     }
 
-    // check to make sure a letter was typed.
-    // Because alpha is a SET, we can use
-    // the built in method .has() to check
-    // if the set contains the key just pressed.
     if (alpha.has( evt.key.toUpperCase() )) {
-        console.log(evt.key.toUpperCase());
+        // console.log(evt.key.toUpperCase());
+        if (tries[attempt].length < 5) {
+            tries[attempt].push( evt.key.toUpperCase() )
+        }
     } else {
-        // fun this if a non-letter key was pressed.
         console.log('not a letter');
     }
+    render();
 }
 
-// this is an array with three arrays inside it.
 const keyboard = [
-    // .split('') creates an array from the string
-   "QWERTYUIOP".split(''), // ["Q", "W", "E", ... "P"]
+   "QWERTYUIOP".split(''),
    "ASDFGHJKL".split(''), 
    "ZXCVBNM".split('')
 ];
 keyboard[2].unshift('ENTER');
 keyboard[2].push('BACK');
 
-
 function render() {
-    // This searches the HTML doc for 
-    // <main id="root"></main>
-    // then saves it in JS
     const main = document.querySelector('#root');
-    let template = `<div class="keyboard">`;
+    let board = `<div class="board">`;
+
+    for (let i=0; i<tries.length; i++) {
+        for (let j=0; j<5; j++) {
+            board += `<div>${ tries[i][j] ? tries[i][j] : "" }</div>`;
+        }
+    }
+    board += `</div>`;
+
+    let keyTemplate = `<div class="keyboard">`;
     
     for (let i = 0; i<keyboard.length; i++) {
-        template += `<div class="row">`;
+        keyTemplate += `<div class="row">`;
         for (let j=0; j<keyboard[i].length; j++) {
-            template += `<div class="key">${ keyboard[i][j] }</div>`
+            keyTemplate += `<div class="key">${ keyboard[i][j] }</div>`
         }
-        template += `</div>`;
+        keyTemplate += `</div>`;
     }
 
-    template += `</div>`;
+    keyTemplate += `</div>`;
     
+    let template = board + keyTemplate;
+
     main.innerHTML = template;
 
     console.log(main);
